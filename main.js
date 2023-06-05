3aada851428da60f8141ec92270a332706c71e18
Den <den@Noutbuk-Denis.local>
2023-06-05 15:25:10 +0300

    first commit feature/bug

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-let count= document.getElementById('count');
+let count = document.getElementById('count');
 
-function increment(){
-document.getElementById('count').innerHTML=String(Number(count.innerHTML)+1);
+function increment() {
+    document.getElementById('count').innerHTML = String(Number(count.innerHTML) + 1);
 }
-function decrement (){
-    document.getElementById('count').innerHTML=String(Number(count.innerHTML)-1);
+function decrement() {
+    document.getElementById('count').innerHTML = String(Number(count.innerHTML) - 1);
 }
 console.log(count)
 
 console.log('main');
-console.log ("dec commit0");
-console.log ("dec commit1");
-console.log ("dec commit2");
-console.log ("dec commit3");
-console.log ("dec commit4");
-console.log ("dec commit555555");
+console.log("dec commit0");
+console.log("dec commit1");
+console.log("dec commit2");
+console.log("dec commit3");
+console.log("dec commit4");
+console.log("dec commit555555");
+
+
+console.log("feature"); 
+console.log("feature1"); 
+console.log("feature2");
