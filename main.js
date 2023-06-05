bb14f3f364a75808d9f683d976b73a731aa80ec1
Den <den@Noutbuk-Denis.local>
2023-06-05 05:46:15 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let count= document.getElementById('count');
+
+function increment(){
+document.getElementById('count').innerHTML=String(Number(count.innerHTML)+1);
+}
+function decrement (){
+    document.getElementById('count').innerHTML=String(Number(count.innerHTML)-1);
+}
+console.log(count)
+console.log("new commit")
+console.log("new commit 2-123-")
